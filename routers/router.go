8b2a7b158886dacb2daa7604b1714c849b5db20e
rulerhuang <rulerhuang@163.com>
2020-10-12 16:24:37 +0800

    增加jwt插件

--- routers/router.go
+++ routers/router.go
@@ -2,8 +2,10 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"learn.gin/middleware/jwt"
 	"learn.gin/pkg/setting"
 	"learn.gin/routers/api/article"
+	"learn.gin/routers/api/auth"
 	"learn.gin/routers/api/tag"
 )
 
@@ -12,21 +14,28 @@ func helloHandler(c *gin.Context) {
 }
 
 func InitRouter() *gin.Engine {
+	gin.SetMode(setting.RunMode)
+
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
-	gin.SetMode(setting.RunMode)
 
+	/*
+		免登陆路由
+	*/
 	r.GET("/", helloHandler)
-	// 路由组
-	apis := r.Group("/api/v1")
+	r.GET("/auth", auth.GetAuth)
 
+	/*
+		登录路由
+	*/
+	apis := r.Group("/api/v1")
+	apis.Use(jwt.JwtMiddle()) // 加载登录校验的中间件
 	// 文章标签
 	apis.GET("/tags", tag.GetTags)
 	apis.POST("/tags", tag.AddTag)
 	apis.PUT("/tags/:id", tag.EditTag)
 	apis.DELETE("/tags/:id", tag.DeleteTag)
-
 	// 文章
 	apis.GET("/article", article.GetArticle)
 	apis.GET("/articles", article.GetArticles)
