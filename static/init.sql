832ad6cd58d1c4b04a3487d088cca1167c63be38
rulerhuang <rulerhuang@163.com>
2020-10-12 16:24:55 +0800

    调整数据库字段

--- static/init.sql
+++ static/init.sql
@@ -32,7 +32,7 @@ CREATE TABLE `blog_article` (
 DROP TABLE IF EXISTS blog_auth;
 CREATE TABLE `blog_auth` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT '' COMMENT '账号',
+  `user_name` varchar(50) DEFAULT '' COMMENT '账号',
   `password` varchar(50) DEFAULT '' COMMENT '密码',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
