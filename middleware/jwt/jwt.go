6f8d49e280a6763a27f19444da24865c0a22956b
rulerhuang <rulerhuang@163.com>
2020-11-12 02:31:01 +0000

    调整默认参数

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -19,7 +19,7 @@ func JwtMiddle() gin.HandlerFunc {
 
 		token := ctx.Query("token")
 		if token == "" {
-			code = e.INVALID_PARAMS
+			code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
 		} else {
 			claims, err := util.ParseToken(token)
 			if err != nil {
